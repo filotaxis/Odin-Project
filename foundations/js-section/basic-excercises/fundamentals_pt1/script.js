3b26b83503275427a99ec06c23d2aecf663c7867
Bryan <bryan.anidem@gmail.com>
2021-09-17 16:44:17 -0500

    completed fundamentals pt 3

---
+++ foundations/js-section/basic-excercises/fundamentals_pt1/script.js
@@ -0,0 +1,10 @@
+// why I even doing this . - .
+
+const add7 = (num) => num + 7;
+const multiply = (num1, num2) => num1 * num2;
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase();
+const lastLetter = (str) => str.slice(-1);
+
+
+console.log(capitalize("RAyitAs"));
+console.log(lastLetter("RAyitAs"));
