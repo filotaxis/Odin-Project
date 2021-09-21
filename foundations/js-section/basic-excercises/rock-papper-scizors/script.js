cfb92a7f59f597aebe49bfe42c83bbdfed102dd8
Bryan <bryan.anidem@gmail.com>
2021-09-21 13:27:19 -0500

    added a basic ui

--- foundations/js-section/basic-excercises/rock-papper-scizors/script.js
+++ foundations/js-section/basic-excercises/rock-papper-scizors/script.js
@@ -1,3 +1,10 @@
+const buttons = document.querySelectorAll("button");
+const winner = document.querySelector(".winner");
+const playerSel = document.querySelector(".player-selection");
+const playerScore = document.querySelector(".player-score");
+const computerSel = document.querySelector(".computer-selection");
+const computerScore = document.querySelector(".computer-score");
+const gameRes = document.querySelector("#game-result");
 const options = ["papper", "rock", "scissors"];
 
 const computerPlay = () => {
@@ -5,37 +12,79 @@ const computerPlay = () => {
     return options[prob];
 }
 
-const singleRound = (player, computer) => {
+const rules = (player, computer) => {
+    
     if (player === computer) {
         return "draw";
     }
     else if (player === "papper" && computer === "rock") {
-        return "player wins";
+        return "player";
     }
     else if (player === "papper" && computer === "scissors") {
-        return "computer wins";
+        return "computer";
     }
     else if (player === "rock" && computer === "scissors") {
-        return "player wins";
+        return "player";
     }
     else if (player === "rock" && computer === "papper") {
-        return "computer wins";
+        return "computer";
     }
     else if (player === "scissors" && computer === "papper") {
-        return "player wins";
+        return "player";
     }
     else if (player === "scissors" && computer === "rock") {
-        return "computer wins";
+        return "computer";
     }
 }
 
-const game = () => {
-    for (let i = 0; i < 5; i++){
-        const player = prompt("enter your guess").toLowerCase();
-        const computer = computerPlay();
-        console.log(`🤷 ${player} vs 💻 ${computer}`);
-        console.log(singleRound(player, computer));
-    }
+// const game = (rounds) => {
+//     for (let i = 0; i < rounds; i++){
+//         // const player = prompt("enter your guess").toLowerCase();
+//         const computer = computerPlay();
+//         console.log(`🤷 ${player} vs 💻 ${computer}`);
+//         console.log(singleRound(player, computer));
+//     }
+// }
+
+const singleRound = () => {
+    let score_p = 0;
+    let score_c = 0;
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const player = button.id;
+            const computer = computerPlay();
+            
+            playerSel.textContent = player;
+            computerSel.textContent = computer;
+
+            const win = rules(player, computer);
+            // winner.textContent = win + " wins!";
+            
+            if (win === "player") {
+                score_p++;
+                winner.textContent = ">";
+            } else if (win === "computer") {
+                score_c++;
+                winner.textContent = "<";
+            } else if (win === "draw") {
+                winner.textContent = "=";
+                
+            }
+
+            playerScore.textContent = score_p;
+            computerScore.textContent = score_c;
+
+            if (score_p >= 3) {
+                gameRes.textContent = "Player! 🤑"
+            }
+            else if (score_c >= 3) {
+                gameRes.textContent = "Mr. Roboto! 🤖"
+            }
+        });
+    });
 }
 
-game();
+singleRound();
+
+
+
